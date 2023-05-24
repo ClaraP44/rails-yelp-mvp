d1091b05ab9f48b35312ae877f02babf7d59418b
clara pecot <clara.pecot44@gmail.com>
2023-05-24 16:05:48 +0200

    yelp mvp - done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::BossaNova.song,
+    address: Faker::Address.country,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+end
+
+
+
+# epicure = Restaurant.new(name: "Epicure", address: "75008 Paris", category: "french")
+# epicure.save
+
+# canberra = Restaurant.new(name: "canberra", address: "10008 Grenoble", category: "chinese")
+# canberra.save
+
+# bruxelles = Restaurant.new(name: "bruxelles", address: "10008 Bruxelles", category: "belgian")
+# bruxelles.save
+
+# roma = Restaurant.new(name: "roma", address: "10008 Rome", category: "italian")
+# roma.save
+
+# tokyo = Restaurant.new(name: "tokyo", address: "10008 tokyo", category: "japanese")
+# tokyo.save
